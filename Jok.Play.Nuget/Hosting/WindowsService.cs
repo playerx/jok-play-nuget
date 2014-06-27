b26c91b3d9c2cf14da96d80e4c2dd9707333e23f
Ezeki Zibzibadze <ez@jok.io>
2014-06-27 19:23:25 +0400

    added windows service for hosting

---
+++ Jok.Play.Nuget/Hosting/WindowsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Jok.Play.Hosting
+{
+    public partial class WindowsService : System.ServiceProcess.ServiceBase
+    {
+        public Action OnStartAction { get; set; }
+        public Action OnStopAction { get; set; }
+
+        public WindowsService()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            if (OnStartAction != null)
+                OnStartAction();
+        }
+
+        protected override void OnStop()
+        {
+            if (OnStopAction != null)
+                OnStopAction();
+        }
+
+        public static void Run(string Name, Action OnStart)
+        {
+            System.ServiceProcess.ServiceBase.Run(new WindowsService { ServiceName = Name, OnStartAction = OnStart });
+        }
+    }
+}
